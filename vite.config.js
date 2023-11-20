0aeb1dd51810bdef53aa8e50e60da6d19e3c8da4
SrGeneroso <5541794+SrGeneroso@users.noreply.github.com>
2023-11-20 22:33:25 +0100

    another ghpages fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { manifestConfig } from "./pwa.config"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? process.env.REPO_NAME : '/',
   plugins: [
     svelte(),
     VitePWA({
