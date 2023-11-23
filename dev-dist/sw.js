8b7e1f6a98b1afba990424d66633645028a917d5
SrGeneroso <5541794+SrGeneroso@users.noreply.github.com>
2023-11-23 11:13:15 +0100

    trying conditional vite base path for ghpages

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab7aa862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.i17vl9c9lcg"
+    "revision": "0.sm2p4ene22o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
