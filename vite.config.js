b4164dcb673fef9369a27480ed68977ee3402855
SrGeneroso <5541794+SrGeneroso@users.noreply.github.com>
2023-11-23 20:07:11 +0100

    revert boy

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ import { manifestConfig } from './pwa.config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:
-    process.env.NODE_ENV === 'production' ? `/${process.env.REPO_NAME}/` : '/',
+  base: '/sveltepwa/', //deploying to github pages needs an extra path
   plugins: [
     svelte(),
     VitePWA({
